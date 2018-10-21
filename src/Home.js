8e4d16c0cf16aa0d1427aa576d403f28a4cd39eb
J Kaufman <jkaufman@Js-MacBook-Pro.local>
2018-10-21 14:06:54 -0400

    minor style changes

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,14 @@ class Home extends Component {
   render () {
     return(
       <div className="home">
-        <h1> Home page</h1>
+        <div className="level">
+          <img src="https://i.imgur.com/xcUlDp1.png"/>
+          <p> Welcome to error tree, a resource for debugging inspired by fault tree analysis. You can learn more using the resources below, or dive in by navigating to "Board"</p>
+          <p><a href="https://en.wikipedia.org/wiki/Fault_tree_analysis">Wikipedia</a> |
+             <a href="http://asq.org/quality-progress/2002/03/problem-solving/what-is-a-fault-tree-analysis.html">ASQ.org</a> |
+             <a href="https://quality-one.com/fta/">Quality-One</a></p>
+
+        </div>
       </div>
     )
   }
